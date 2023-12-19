96dc8ef477c83c866f1588263a57eb0557344c3c
Martin Mirchev <mirchevmartin2203@gmail.com>
2023-12-20 01:04:25 +0200

    Reformat and fix a bug

--- repair.py
+++ repair.py
@@ -23,11 +23,17 @@ Here is the code:
 {program}
 """
 
-prompt_number = 0 
+prompt_number = 0
+
 
 def send_prompt(
-    args, text: str, model: str, num=1, temperature=0.8, reference=None,
-    bug_description=None
+    args,
+    text: str,
+    model: str,
+    num=1,
+    temperature=0.8,
+    reference=None,
+    bug_description=None,
 ):
     global prompt_number
     # print(text)
@@ -58,15 +64,17 @@ def send_prompt(
 
     prompt_number += 1
     if args.debug:
-        with(open(join(args.output, "prompt_{}.json".format(prompt_number)), "w")) as f:
+        with open(join(args.output, "prompt_{}.json".format(prompt_number)), "w") as f:
             json.dump(messages, f, indent=4)
 
-    return client.chat.completions.create(model=model,
-    messages=messages,
-    temperature=temperature,
-    max_tokens=2048,
-    top_p=1.0,
-    n=num)
+    return client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+        max_tokens=2048,
+        top_p=1.0,
+        n=num,
+    )
 
 
 file_error_log = "error.logs"
@@ -102,7 +110,9 @@ def build(build_script, debug=False):
 
 def test(test_script, args=None, debug=False, env=dict()):
     res = execute_command(
-        test_script + " " + args if args is not None else " ", show_output=debug, env=env
+        test_script + " " + args if args is not None else " ",
+        show_output=debug,
+        env=env,
     )
     return res == 0
 
@@ -183,33 +193,51 @@ def parse_args():
         required=False,
     )
 
-    #optional.add_argument(
+    # optional.add_argument(
     #    "-context", help="Context Window size", type=int, required=False, default=-1
-    #)
+    # )
 
     optional.add_argument(
         "-reference", help="Reference file", type=str, required=False, default=None
     )
 
-    optional.add_argument("-passing-tests", help="Comma sepearted list of test identifiers for passing tests", type=str, required=False, default ="")
-    optional.add_argument("-failing-tests", help="Comma sepearted list of test identifiers for failing tests", type=str, required=False, default="")
-
+    optional.add_argument(
+        "-passing-tests",
+        help="Comma sepearted list of test identifiers for passing tests",
+        type=str,
+        required=False,
+        default="",
+    )
+    optional.add_argument(
+        "-failing-tests",
+        help="Comma sepearted list of test identifiers for failing tests",
+        type=str,
+        required=False,
+        default="",
+    )
 
     optional.add_argument(
         "-fl", help="Fault localization path", type=argparse.FileType("r")
     )
 
     optional.add_argument(
-        "-binary-loc",help="Location of the binary", type=str, required=False, default=None
+        "-binary-loc",
+        help="Location of the binary",
+        type=str,
+        required=False,
+        default=None,
     )
 
     optional.add_argument(
-        "-do-fl", help="Whether to do fault localization", action="store_true",default=False
+        "-do-fl",
+        help="Whether to do fault localization",
+        action="store_true",
+        default=False,
     )
 
-    optional.add_argument("-fl-formula",help="Fault localization formula", type=str, default="Ochiai")
-
-
+    optional.add_argument(
+        "-fl-formula", help="Fault localization formula", type=str, default="Ochiai"
+    )
 
     optional.add_argument(
         "--project-path", help="Project path", type=str, required=False, default=None
@@ -236,7 +264,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def repair(args,file,fault_info):
+def repair(args, file, fault_info):
     lang_info = "in the {} programming language".format(
         args.language if args.language else file(".")[-1]
     )
@@ -282,18 +310,20 @@ def repair(args,file,fault_info):
             i = i + 1
     return results
 
-def make_patch(args, file,patched_path,i):
+
+def make_patch(args, file, patched_path, i):
     os.system(f"diff -u {file} {patched_path} > {args.output}/patches/patch_{i}.diff")
 
+
 conversion_table = {
-    'failed': 1,
-    'noncompiling': 0,
-    'implausible': -1,
-    'plausible': -2,
+    "failed": 1,
+    "noncompiling": 0,
+    "implausible": -1,
+    "plausible": -2,
 }
 
 
-def process_response(resp,file, args, i):
+def process_response(resp, file, args, i):
     with open(os.path.join(args.output, "response_{}.txt".format(i)), "w") as f:
         f.write(resp.message.content)
     # if args.debug:
@@ -326,95 +356,164 @@ def process_response(resp,file, args, i):
         else:
             print("Patch {} does not compile".format(i))
             shutil.copy(patched_path, patched_path + "_noncompiling")
-            return ('noncompiling',file, patched_path)
+            return ("noncompiling", file, patched_path)
     else:
         built = True
-    
+
     if built and args.test:
         plausible = True
         if args.passing_tests or args.failing_tests:
-            for test_id in [*args.passing_tests.split(","),*args.failing_tests.split(",")]:
+            for test_id in [
+                *args.passing_tests.split(","),
+                *args.failing_tests.split(","),
+            ]:
                 if test_id:
                     if not test(args.test, test_id, args.debug):
                         print("Patch {} failed for test {}".format(i, test_id))
                         shutil.copy(patched_path, patched_path + "_implausible")
                         plausible = False
-                        return ('implausible',file, patched_path)
+                        return ("implausible", file, patched_path)
         else:
             if not test(args.test, None, args.debug):
                 print("Patch {} failed test script".format(i))
                 plausible = False
                 shutil.copy(patched_path, patched_path + "_implausible")
-                return ('implausible',file, patched_path)
+                return ("implausible", file, patched_path)
         if plausible:
             print("Patch {} is Plausible".format(i))
             shutil.copy(patched_path, patched_path + "_plausible")
-            return ('plausible',file, patched_path)
-    return ('failed',file, patched_path)
+            return ("plausible", file, patched_path)
+    return ("failed", file, patched_path)
 
 
 def do_fault_localization_py(args):
-    execute_command("python3 -m pytest --src . --family sbfl --exclude \"[$(ls | grep test | grep .py | tr '\n' ',' | sed 's/,$//')]\"",directory=args.project_path)
+    execute_command(
+        "python3 -m pytest --src . --family sbfl --exclude \"[$(ls | grep test | grep .py | tr '\n' ',' | sed 's/,$//')]\"",
+        directory=args.project_path,
+    )
     report_dir = os.path.dirname(args.project_path)
     for dir in os.listdir(report_dir):
         if dir.startswith("FauxPyReport"):
-            if not os.path.exists(join(report_dir ,dir, 'Scores_{}.csv'.format(args.fl_formula) )):
-                print("Fault localization report for formula {} does not exist".format(args.fl_formula))
+            if not os.path.exists(
+                join(report_dir, dir, "Scores_{}.csv".format(args.fl_formula))
+            ):
+                print(
+                    "Fault localization report for formula {} does not exist".format(
+                        args.fl_formula
+                    )
+                )
                 exit(1)
-            with open(join(report_dir ,dir, 'Scores_{}.csv'.format(args.fl_formula) )) as f:
+            with open(
+                join(report_dir, dir, "Scores_{}.csv".format(args.fl_formula))
+            ) as f:
                 distribution = {}
                 for line in f.readlines()[1:]:
-                    #print(line)
-                    path,line_and_probability = line.split('::')
-                    line,_ = line_and_probability.split(',')
-                    distribution[path] = distribution.get(path,[]) + [line]
+                    # print(line)
+                    path, line_and_probability = line.split("::")
+                    line, _ = line_and_probability.split(",")
+                    distribution[path] = distribution.get(path, []) + [line]
                 return process_distribution(args, distribution)
     raise Exception("No fault localization report found")
 
+
 def do_fault_localization_c(args):
     if not args.binary_loc or not args.file:
-        print("Please provide the location of the binary and the path of the file for the binary if you want to do FL on C")
+        print(
+            "Please provide the location of the binary and the path of the file for the binary if you want to do FL on C"
+        )
         exit(1)
-    
-    os.makedirs(join(args.output,'passing_traces'),exist_ok=True)
-    os.makedirs(join(args.output,'failing_traces'),exist_ok=True)
 
-    execute_command("bash -c 'python3 /sbfl/dump_lines.py {0} $(cat {0} | wc -l) > /sbfl/lines.txt'".format(join(args.project_path,args.file)))
+    os.makedirs(join(args.output, "passing_traces"), exist_ok=True)
+    os.makedirs(join(args.output, "failing_traces"), exist_ok=True)
 
-    execute_command("python3 /sbfl/instrument.sh {} /sbfl/lines.txt".format(join(args.project_path,args.binary_loc)),directory='/sbfl/')
+    execute_command(
+        "bash -c 'python3 /sbfl/dump_lines.py {0} $(cat {0} | wc -l) > /sbfl/lines.txt'".format(
+            join(args.project_path, args.file)
+        )
+    )
 
-    execute_command("bash -c 'mv /sbfl/*.tracer {}'".format(join(args.project_path,args.binary_loc)))
+    execute_command(
+        "python3 /sbfl/instrument.sh {} /sbfl/lines.txt".format(
+            join(args.project_path, args.binary_loc)
+        ),
+        directory="/sbfl/",
+    )
+
+    execute_command(
+        "bash -c 'mv /sbfl/*.tracer {}'".format(
+            join(args.project_path, args.binary_loc)
+        )
+    )
 
     for passing_test_id in args.passing_tests.split(","):
         if passing_test_id:
-            test(args.test,passing_test_id,args.debug,env={"TRACE_FILE": join(args.output,'passing_traces',f'passing_trace_{passing_test_id}') })
+            test(
+                args.test,
+                passing_test_id,
+                args.debug,
+                env={
+                    "TRACE_FILE": join(
+                        args.output,
+                        "passing_traces",
+                        f"passing_trace_{passing_test_id}",
+                    )
+                },
+            )
     for failing_test_id in args.failing_tests.split(","):
         if failing_test_id:
-            test(args.test,failing_test_id,args.debug,env={"TRACE_FILE": join(args.output,'failing_traces',f'failing_trace_{failing_test_id}')})
+            test(
+                args.test,
+                failing_test_id,
+                args.debug,
+                env={
+                    "TRACE_FILE": join(
+                        args.output,
+                        "failing_traces",
+                        f"failing_trace_{failing_test_id}",
+                    )
+                },
+            )
 
-    if os.listdir(join(args.output,'passing_traces')) == 0 and os.listdir(join(args.output,'failing_traces')) == 0:
+    if (
+        os.listdir(join(args.output, "passing_traces")) == 0
+        and os.listdir(join(args.output, "failing_traces")) == 0
+    ):
         print("No traces were generated")
         exit(1)
 
-    execute_command(f"python3 /sbfl/sbfl.py {join(args.output,'failing_traces')} {join(args.output,'passing_traces')}",directory='/sbfl/')
+    execute_command(
+        f"python3 /sbfl/sbfl.py {join(args.output,'failing_traces')} {join(args.output,'passing_traces')}",
+        directory="/sbfl/",
+    )
 
-    with open(join('/sbfl/ochiai.csv','r')) as f:
+    with open("/sbfl/ochiai.csv", "r") as f:
         distribution = {}
         for line in f.readlines():
-            path,line_and_prob = line.split(':')
-            line,prob = line_and_prob.split(',')
-            distribution[path] = distribution.get(path,[]) + [line]
+            path, line_and_prob = line.split(":")
+            line, prob = line_and_prob.split(",")
+            distribution[path] = distribution.get(path, []) + [line]
         return process_distribution(args, distribution)
 
 
 def do_fault_localization_java(args):
-    execute_command("java -cp '/flacoco/target/flacoco-1.0.7-SNAPSHOT-jar-with-dependencies.jar' fr.spoonlabs.flacoco.cli.FlacocoMain --projectpath {} -o flacoco.run".format(args.project_path),directory=args.project_path)
-    with open(join(args.project_path,'flacoco.run')) as f:
+    execute_command(
+        "java -cp '/flacoco/target/flacoco-1.0.7-SNAPSHOT-jar-with-dependencies.jar' fr.spoonlabs.flacoco.cli.FlacocoMain --projectpath {} -o flacoco.run".format(
+            args.project_path
+        ),
+        directory=args.project_path,
+    )
+    with open(join(args.project_path, "flacoco.run")) as f:
         distribution = {}
         for line in f.readlines():
-            path,line,_= line.split(',')
-            path = join(args.project_path,'src','main','java',path.replace('.','/') + '.java')
-            distribution[path] = distribution.get(path,[]) + [line]
+            path, line, _ = line.split(",")
+            path = join(
+                args.project_path,
+                "src",
+                "main",
+                "java",
+                path.replace(".", "/") + ".java",
+            )
+            distribution[path] = distribution.get(path, []) + [line]
         return process_distribution(args, distribution)
 
 
@@ -431,40 +530,60 @@ def fault_localization(args):
         print("Unsupported language {}".format(args.language))
         exit(1)
 
+
 def process_distribution(args, distribution):
     distribution_converted = []
-    for k,v in distribution.items():
+    for k, v in distribution.items():
         if args.lines:
             with open(k) as f:
                 lines = f.readlines()
-                distribution_converted.append((k,'in any of the lines in the list [{}]'.format(','.join(list(map(lambda x: lines[int(x)-1],v))))))
+                distribution_converted.append(
+                    (
+                        k,
+                        "in any of the lines in the list [{}]".format(
+                            ",".join(list(map(lambda x: lines[int(x) - 1], v)))
+                        ),
+                    )
+                )
         else:
-            distribution_converted.append((k,'in any of the lines in the list {}'.format(','.join(v))))
+            distribution_converted.append(
+                (k, "in any of the lines in the list {}".format(",".join(v)))
+            )
     return distribution_converted
+
 
 def get_bug_info(args):
     if args.do_fl:
         print("doing FL")
-        required_args = [ ('language',args.language),('project path',args.project_path),('test_script',args.test)]
-        for _,val in required_args:
+        required_args = [
+            ("language", args.language),
+            ("project path", args.project_path),
+            ("test_script", args.test),
+        ]
+        for _, val in required_args:
             if not val:
-                print(f"Please provide {list(map(lambda x: x[0],required_args))} if you want fault localization")
+                print(
+                    f"Please provide {list(map(lambda x: x[0],required_args))} if you want fault localization"
+                )
                 exit(1)
         return fault_localization(args)
     elif args.fl:
         print("Provided Fault localization is not supported currently")
         sys.exit(1)
-        #fl_info = list(map(lambda line: line.split("@")[-1], args.fl.readlines()))
-        #if args.lines:
+        # fl_info = list(map(lambda line: line.split("@")[-1], args.fl.readlines()))
+        # if args.lines:
         #    fl_info = list(
         #        map(lambda line: "`" + file_contents[line - 1] + "`", fl_info)
         #    )
-        #bug_info = "in any of the lines in the list {}".format(",".join(fl_info))
+        # bug_info = "in any of the lines in the list {}".format(",".join(fl_info))
     elif args.file:
-        return [(args.file,"in the file ")]
+        return [(args.file, "in the file ")]
     else:
-        print("Please provide a way to find the file to repair - either fault localization (manual, provided) or file path")
+        print(
+            "Please provide a way to find the file to repair - either fault localization (manual, provided) or file path"
+        )
         sys.exit(1)
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -479,10 +598,10 @@ if __name__ == "__main__":
     patches = []
 
     for file_path, fault_info in cases:
-        for patch in repair(args,file_path,fault_info):
-            transformed_patch = (conversion_table[patch[0]],*patch[1:])
-            heapq.heappush(patches,patch)
-    
-    for i in range(min(args.top,len(patches))):
-        (priority,file,patched_path) = heapq.heappop(patches)
-        make_patch(args, file,patched_path,i)
+        for patch in repair(args, file_path, fault_info):
+            transformed_patch = (conversion_table[patch[0]], *patch[1:])
+            heapq.heappush(patches, patch)
+
+    for i in range(min(args.top, len(patches))):
+        (priority, file, patched_path) = heapq.heappop(patches)
+        make_patch(args, file, patched_path, i)
